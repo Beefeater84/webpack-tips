313b1c871c540ff527991a3e583fb1e2606017cb
Beefeater84 <site2max@gmail.com>
2021-05-04 10:25:28 +0300

    + app,jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hello, I am React component</h1>
+  </React.StrictMode>,
+  document.querySelector('.container')
+);
+
+
